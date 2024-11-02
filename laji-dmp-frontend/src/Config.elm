ca10cd9ee085bf5682fa5ff28de1dd1bb8ebcccc
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2024-11-02 20:21:03 +0200

    get dmp list from api

---
+++ laji-dmp-frontend/src/Config.elm
@@ -0,0 +1,12 @@
+module Config exposing (..)
+
+type alias Config =
+  { dmpApiUrl: String
+  , lajiApiUrl: String
+  }
+
+config : Config
+config =
+  { dmpApiUrl = "http://localhost:4000/"
+  , lajiApiUrl = ""
+  }
--- laji-dmp-frontend/src/Pages/DmpIndex.elm
+++ laji-dmp-frontend/src/Pages/DmpIndex.elm
@@ -1,4 +1,6 @@
 module Pages.DmpIndex exposing (..)
+
+import Http
 import Browser
 import Html.Attributes exposing (href)
 import Html exposing (a)
@@ -8,26 +10,68 @@ import Html exposing (ul)
 import Html exposing (li)
 import Html exposing (Html)
 import Html exposing (div)
+import Config exposing (config)
+import Json.Decode
+import Json.Decode.Pipeline
+import Array
+import Platform.Cmd as Cmd
+import Platform.Cmd as Cmd
 
-type alias Model = {}
+type Model = Loading | Error | DmpList DmpListResponse
 
-type Msg = Empty
+type Msg = GotDmpListResponse (Result Http.Error DmpListResponse)
+
+type alias DmpListResponseElement =
+  { id: Int
+  , testField: String
+  }
+
+type alias DmpListResponse = Array.Array DmpListResponseElement
+
+dmpListElementDecoder : Json.Decode.Decoder DmpListResponseElement
+dmpListElementDecoder =
+  Json.Decode.succeed DmpListResponseElement
+    |> Json.Decode.Pipeline.required "plan_id" Json.Decode.int
+    |> Json.Decode.Pipeline.required "test_field" Json.Decode.string
+
+dmpListDecoder : Json.Decode.Decoder DmpListResponse
+dmpListDecoder = Json.Decode.array dmpListElementDecoder
+
+getDmpList : (Result Http.Error DmpListResponse -> msg) -> Cmd msg
+getDmpList msg =
+  Http.get
+    { url = config.dmpApiUrl ++ "dmp"
+    , expect = Http.expectJson msg dmpListDecoder
+    }
 
 init : ( Model, Cmd Msg )
-init = ({}, Cmd.none)
+init = (Loading, getDmpList GotDmpListResponse)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  (model, Cmd.none)
+  case msg of
+    GotDmpListResponse res ->
+      case res of
+        Ok dmpList -> (DmpList dmpList, Cmd.none)
+        Err e ->
+          let _ = Debug.log "Error loading DMP list" e
+          in (Error, Cmd.none)
+
+dmpListElementView : DmpListResponseElement -> Html msg
+dmpListElementView elem =
+  li [] [ a [href <| "dmp/" ++ String.fromInt elem.id] [text elem.testField] ]
 
 view : Model -> { title : String, body : Html Msg }
 view model =
   { title = "Dmp Index View"
   , body = div []
-    [ ul []
-      [ li []
-        [ a [href "/dmp/123"] [text "DMP 123"] ]
-      ]
-    , a [href "/dmp/new"] [text "New DMP"]
-    ]
+    <| case model of
+      Error -> [ text "Failed to load the list of DMPs." ]
+      Loading -> [ text "Loading the list of DMPs..." ]
+      DmpList dmpList -> 
+        [
+          ul []
+            <| Array.toList <| Array.map dmpListElementView dmpList
+          , a [href "/dmp/new"] [text "New DMP"]
+        ]
   }
