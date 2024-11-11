e194d7734ad1680e26341a2ce4c9dccca4dc191e
Pyry Mäkinen <pyry.makinen@helsinki.fi>
2024-11-12 00:42:38 +0200

    get existing dmp

--- laji-dmp-frontend/src/Pages/DmpEdit.elm
+++ laji-dmp-frontend/src/Pages/DmpEdit.elm
@@ -10,8 +10,13 @@ import Views.DmpEditor exposing (ModelStatus(..))
 import DmpApi exposing (deleteDmp)
 import Http exposing (Error)
 import Views.Dialog exposing (dialog)
+import DmpApi exposing (DataManagementPlan)
+import DmpApi exposing (getDmp)
 
-type alias Model = Views.DmpEditor.Model
+type Model
+  = EditorModel Views.DmpEditor.Model
+  | Loading Nav.Key String
+  | Error
 
 type Msg
   = EditorMsg Views.DmpEditor.Msg
@@ -19,40 +24,64 @@ type Msg
   | OnConfirmDelete
   | OnCancelDelete
   | GotDmpDeleteResponse (Result Error String)
+  | GotDmpGetResponse (Result Http.Error DataManagementPlan)
 
 deleteDialogId : String
 deleteDialogId = "delete-dialog"
 
 init : Nav.Key -> String -> ( Model, Cmd Msg )
-init key id = Tuple.mapSecond (Cmd.map EditorMsg) <| Views.DmpEditor.init key <| Edit id
+init key strId =
+  let maybeId = String.toInt strId
+  in case maybeId of
+    Just id ->
+      (Loading key strId, getDmp id GotDmpGetResponse)
+    Nothing -> (Error, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = case msg of
-  EditorMsg subMsg -> Tuple.mapSecond (Cmd.map EditorMsg) <| Views.DmpEditor.update subMsg model
-  OnDelete -> (model, Cmd.none)
-  OnCancelDelete -> (model, Cmd.none)
-  OnConfirmDelete ->
-    ( { model | status = Submitting }
-    , case model.mode of
+update msg model = case (msg, model) of
+  (EditorMsg subMsg, EditorModel subModel) -> Tuple.mapBoth (\m -> EditorModel m) (Cmd.map EditorMsg) <| Views.DmpEditor.update subMsg subModel
+  (OnDelete, EditorModel _) -> (model, Cmd.none)
+  (OnCancelDelete, EditorModel _) -> (model, Cmd.none)
+  (OnConfirmDelete, EditorModel subModel) ->
+    ( EditorModel { subModel | status = Submitting }
+    , case subModel.mode of
       Edit id -> deleteDmp id GotDmpDeleteResponse
       _ -> Debug.log "Error: tried to delete DMP while not in edit mode" Cmd.none
     )
-  GotDmpDeleteResponse res -> case res of
+  (GotDmpDeleteResponse res, EditorModel subModel) -> case res of
     Ok str -> 
-      (model, Nav.pushUrl model.key "/dmp")
+      (model, Nav.pushUrl subModel.key "/dmp")
     Err e ->
-      ({ model | status = SubmitError e }, Cmd.none)
+      (EditorModel { subModel | status = SubmitError e }, Cmd.none)
+  (GotDmpGetResponse res, Loading key strId) ->
+    case res of
+      Ok dmp ->
+        ( EditorModel
+          { dmp = dmp
+          , status = Editing
+          , key = key
+          , mode = Edit strId
+          }
+        , Cmd.none
+        )
+      Err e ->
+        let _ = Debug.log "Error loading DMP" e
+        in (Error, Cmd.none)
+  (_, _) -> (Error, Cmd.none)
 
 view : Model -> { title : String, body : Html Msg }
 view model =
   { title = "DMP Edit View"
-  , body = Html.div []
-    [ Html.map EditorMsg <| Views.DmpEditor.view model
-    , button [ onClick OnDelete ] [text "Delete DMP"]
-    , dialog deleteDialogId []
-      [ text "Do you really want to delete the DMP?"
-      , button [ onClick OnConfirmDelete ] [text "Delete DMP"]
-      , button [ onClick OnCancelDelete ] [text "Cancel"]
+  , body = Html.div [] <| case model of
+    EditorModel subModel ->
+      [ Html.map EditorMsg <| Views.DmpEditor.view subModel
+      , button [ onClick OnDelete ] [text "Delete DMP"]
+      , dialog deleteDialogId []
+        [ text "Do you really want to delete the DMP?"
+        , button [ onClick OnConfirmDelete ] [text "Delete DMP"]
+        , button [ onClick OnCancelDelete ] [text "Cancel"]
+        ]
       ]
-    ]
+    Loading key id -> [text "Loading DMP..."]
+    Error -> [text "Error"]
   }
